304713391c598198239be398db0b39a22dc264eb
Quinten Coret <Qcoret@gmail.com>
2021-07-08 12:53:58 +0200

    Minor

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var msgs = {
     1: {
-        "txt": "<p><b>China pneumonia outbreak: Mystery virus probed in Wuhan (BBC)</b></p><p>Chinese authorities have launched an investigation into a mysterious viral pneumonia which has infected dozens of people in the central city of Wuhan. It comes amid online fears the virus could be linked to Sars, or severe acute respiratory syndrome. The potentially deadly, flu-like Sars virus killed more than 700 people around the world in 2002-03, after originating in China. Local authorities say there has been no human-to-human transmission. However, a number of those infected worked at a seafood market in the city, leading authorities to clean the area.</p>",
+        "txt": "<p><b>China pneumonia outbreak: Mystery virus probed in Wuhan (BBC)</b></p><p>Chinese authorities have launched an investigation into a mysterious viral pneumonia which has infected dozens of people in the central city of Wuhan. It comes amid fears the virus could be linked severe acute respiratory syndrome. The deadly, flu-like Sars virus killed more than 700 people around the world in 2002, after originating in China. Local authorities say there has been no human-to-human transmission. However, a number of those infected worked at a seafood market in the city, leading authorities to clean the area.</p>",
         "btn": [
             ["Volgende.", "next(2);setDate(2020, 1, 6)"]
         ]
     },
     2: {
-        "txt": "<p><b>Tot nu toe 59 gevallen van mysterieuze longziekte in China (NOS)</b></p><p>In de Oost-Chinese miljoenenstad Wuhan zijn tot dusver 59 besmettingen ontdekt van een mysterieuze longziekte. Volgens de WHO is onduidelijk om wat voor ziekte het gaat en waardoor deze wordt veroorzaakt, maar zou het vaststaan dat de ziekte niet lijkt op eerdere uitbraken. Vermoedelijk is het een vorm van griep, maar het is zelfs niet zeker dat het om een virusziekte gaat. Elf mensen zijn ernstig ziek, van de andere zieken is de toestand vooralsnog stabiel.</p>",
+        "txt": "<p><b>Tot nu toe 59 gevallen van mysterieuze longziekte in China (NOS)</b></p><p>In de Oost-Chinese miljoenenstad Wuhan zijn tot dusver 59 besmettingen ontdekt van een mysterieuze longziekte. Volgens de WHO is onduidelijk om wat voor ziekte het gaat en waardoor deze wordt veroorzaakt. Vermoedelijk is het een vorm van griep, maar het is niet zeker dat het om een virusziekte gaat. Elf mensen zijn ernstig ziek, van de andere zieken is de toestand stabiel.</p>",
         "btn": [
             ["Volgende.", "next(3);setDate(2020, 1, 10)"]
         ]
@@ -26,25 +26,25 @@ var msgs = {
         ]
     },
     5: {
-        "txt": "<p><b>China Reports First Death From New Virus (New York Times)</b></p><p> Chinese state media on Saturday reported the first known death from a new virus that has infected dozens of people in China and set off worries across Asia. The Xinhua news agency cited the health commission in the central Chinese city of Wuhan, where the illness first appeared, in reporting the death. The health commission said the patient, a 61-year-old man, died on Thursday night. While no new cases have been detected since Jan. 3, questions remain. Experts have said the minimum incubation period for some infections is 15 days, suggesting that it may be another week before authorities can determine the full extent of the illness.</p>",
+        "txt": "<p><b>China Reports First Death From New Virus (New York Times)</b></p><p> Chinese state media on Saturday reported the first known death from a new virus that has infected dozens of people in China and set off worries across Asia. The health commission said the patient, a 61-year-old man, died on Thursday night. While no new cases have been detected since Jan. 3, questions remain. Experts have said the incubation period for most infections is two weeks, suggesting that it may be another week before authorities can determine the full extent of the illness.</p>",
         "btn": [
             ["Volgende.", "if(getMeasure('warningWuhan')){next(6);setDate(2020,1,13)}else{next(7);setDate(2020,1,14)};"]
         ]
     },
     6: {
-        "txt": "<p><b>Reisadvies Wuhan in China bijgesteld door nieuwe ziekte (Nu.nl)</b><p>Het ministerie van Buitenlandse Zaken maakte vandaag bekend dat reizigers in de stad Wuhan in China extra voorzorgsmaatregelen moeten nemen. Zo'n zestig mensen zijn daar een nieuwe, onbekende ziekte opgelopen, waardoor één persoon is overleden. Chinese wetenschappers vermoeden dat de bron een lokale vismarkt is, maar benadrukken dat de ziekte niet besmettelijk lijkt. Het ministerie raadt reizigers aan om regelmatig hun handen te wassen, en contact met dieren en zieken te vermijden.</p>",
+        "txt": "<p><b>Reisadvies Wuhan in China bijgesteld door nieuwe ziekte (Nu.nl)</b><p>Het ministerie van Buitenlandse Zaken maakte vandaag bekend dat reizigers in de stad Wuhan in China extra voorzorgsmaatregelen moeten nemen. Zo'n zestig mensen zijn daar een nieuwe, onbekende ziekte opgelopen, waardoor één persoon is overleden. Wetenschappers vermoeden dat de bron een lokale vismarkt is, maar benadrukken dat de ziekte niet besmettelijk lijkt. Het ministerie raadt reizigers aan om regelmatig hun handen te wassen, en contact met dieren en zieken te vermijden.</p>",
         "btn": [
             ["Volgende.", "next(7);setDate(2020,1,14);"]
         ]
     },
     7: {
-        "txt": "<p><b>WHO says new Chinese virus could spread, warns hospitals worldwide (Reuters)</b></p><p>There may have been limited human-to-human transmission of a new virus in China within families, and it is possible there could be a wider outbreak, the WHO warned on Tuesday. Yesterday, a Chinese woman was quarantined in Thailand with a mystery strain of coronavirus, the first time the virus has been detected outside China. The WHO is preparing for the possibility that there could be a wider outbreak, Maria Van Kerkhove, acting head of WHO’s emerging diseases unit, told a Geneva news briefing. “It is still early days, we don’t have a clear clinical picture.”</p>",
+        "txt": "<p><b>WHO says new Chinese virus could spread, warns hospitals worldwide (Reuters)</b></p><p>There may have been limited human-to-human transmission of a new virus in China within families, and it is possible there could be a wider outbreak, the WHO warned on Tuesday. This comes after a Chinese woman was quarantined in Thailand with a mystery strain of coronavirus, the first time the virus has been detected outside China. The WHO is preparing for the possibility that there could be a wider outbreak, the acting head of WHO’s emerging diseases unit told a Geneva news briefing. “It is still early days, we don’t have a clear clinical picture.”</p>",
         "btn": [
             ["Volgende.", "next(8);setDate(2020,1,15);"]
         ]
     },
     8: {
-        "txt": "<p>Na afloop van een werkbezoek aan het Martini Ziekenhuis heeft een journalist een vraag over het nieuwe virus. “Minister $NAME, in een persconferentie waarschuwde de Wereldgezondheidsorganisatie gisteren alle ziekenhuizen wereldwijd voor het 2019-nCoV virus. Moeten we ons zorgen maken over een nieuwe epidemie als SARS?”</p>",
+        "txt": "<p>Na afloop van een werkbezoek aan het Martini Ziekenhuis heeft een journalist een vraag over het nieuwe virus. “Minister $NAME, in een persconferentie waarschuwde de Wereldgezondheidsorganisatie gisteren alle ziekenhuizen wereldwijd voor het 2019-nCoV virus. Moeten we ons zorgen maken over een nieuwe epidemie?”</p>",
         "btn": [
             ["Nederland is goed voorbereid."],
             ["Het zijn zorgwekkende berichten."]
